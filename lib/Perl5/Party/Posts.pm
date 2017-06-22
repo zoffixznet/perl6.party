2fbdf6cd6daaeeab3e499db0867dd0e342507394
Zoffix Znet <cpan@zoffix.com>
2017-06-22 12:33:00 -0400

    x

--- lib/Perl5/Party/Posts.pm
+++ lib/Perl5/Party/Posts.pm
@@ -37,7 +37,12 @@ sub load {
                         process_irc($content))));
 
     $content =~ s/^```$//gm;
-    return $meta, $content, markdown $content;
+    my $prefix = '<a href="https://perl6.party/post/' . $post
+        . '" style="display: block; background: #ccc; border-radius: 3px; '
+        . 'font-size: 110%; border: 1px dotted #666; text-align: center; '
+        . 'adding: 10px 5px;">Read this article on Perl6.Party</a>'
+        . "\n\n";
+    return $meta, "$prefix$content", markdown $content;
 }
 
 sub process {
